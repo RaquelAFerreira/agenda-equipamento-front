d5b1703818c5b6f20b42e98fab5009348e9406db
RaquelAFerreira <raquelalmferreira@gmail.com>
2021-01-20 17:15:51 -0300

    Adiciona página de NOT FOUND

--- src/routes/colaborador.routes.js
+++ src/routes/colaborador.routes.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import Colaborador from '../pages/Colaborador';
+import NotFound from '../pages/NotFound';
 
 export default function ColaboradorRoutes() {
     return (
         <BrowserRouter>
             <Switch>
                 <Route exact path="/" component={Colaborador} />
+                <Route exact path="*/" component={NotFound} />
             </Switch>
         </BrowserRouter>
 
--- src/routes/infra.routes.js
+++ src/routes/infra.routes.js
@@ -3,6 +3,7 @@ import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import Inicial from '../pages/Infra/Inicial';
 import HistoricoSolicitacoes from '../pages/Infra/HistoricoSolicitacoes';
 import Equipamentos from '../pages/Infra/Equipamentos';
+import NotFound from '../pages/NotFound';
 
 export default function InfraRoutes() {
     return (
@@ -11,6 +12,8 @@ export default function InfraRoutes() {
                 <Route exact path="/" component={Inicial} />
                 <Route exact path="/historico-solicitacoes" component={HistoricoSolicitacoes} />
                 <Route exact path="/equipamentos" component={Equipamentos} />
+                <Route exact path="*/" component={NotFound} />
+
             </Switch>
         </BrowserRouter>
 
