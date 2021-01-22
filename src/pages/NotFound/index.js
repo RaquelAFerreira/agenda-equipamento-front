b571a0a46b6ad342d5e54679fb8596f436a4208c
RaquelAFerreira <raquelalmferreira@gmail.com>
2021-01-22 10:33:35 -0300

    Estiliza tela de Not Found

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { FiAlertCircle } from "react-icons/fi";
+import { Grid } from '@material-ui/core';
+// import ButtonAppBar from '../../Components/Navbar';
 
-export default function NotFound() {
-    
-    
-    return(
-        <h1>
-            404 NOT FOUND
-        </h1>
+export default function PageNotFound() {
+    return (
+        <div>
+            <Grid container direction="column" justify="center" alignItems="center" style={{marginTop:"15%", position:"fixed"}} spacing={2}>
+            <Grid item>
+            <FiAlertCircle style={{ fontSize: 200, color: "#165091" }}/>
+            </Grid>
+            <h2 style={{ color: "#165091" }} >404 ERROR</h2> 
+        </Grid>
+        </div>
     )
 }
+
